2a23d9df21eeee66f93f0edc3a3ab63d6f5990a8
suryansh9315 <suryansh.sharma9315@gmail.com>
2024-12-26 12:35:01 +0530

    Updated Firebase

--- firebase.py
+++ firebase.py
@@ -5,9 +5,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-service_account_key_path = os.getenv('GOOGLE_CLOUD_SERVICE_ACCOUNT')
-
-cred = credentials.Certificate('./firebase-adminsdk.json')
+cred = credentials.Certificate({
+    "type": os.getenv('FIREBASE_TYPE'),
+    "project_id": os.getenv('FIREBASE_PROJECT_ID'),
+    "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+    "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+    "auth_uri": os.getenv('FIREBASE_AUTH_URI'),
+    "token_uri": os.getenv('FIREBASE_TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('FIREBASE_AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL'),
+    "universe_domain": os.getenv('FIREBASE_UNIVERSE_DOMAIN')
+})
 
 default_app = firebase_admin.initialize_app(cred, {
     'storageBucket': 'brss-8827a.firebasestorage.app'
